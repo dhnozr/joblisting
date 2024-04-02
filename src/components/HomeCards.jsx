8a26c4aeb4476f3e3d98e88adedf4e4a25e7e048
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-04-02 17:01:36 +0300

    Link added

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
 import { Card } from './Card';
 
 export const HomeCards = () => {
@@ -9,19 +9,19 @@ export const HomeCards = () => {
           <Card>
             <h2 className='text-2xl font-bold'>For Developers</h2>
             <p className='mt-2 mb-4'>Browse our React jobs and start your career today</p>
-            <a href='/jobs.html' className='inline-block px-4 py-2 text-white bg-black rounded-lg hover:bg-gray-700'>
+            <Link to='/jobs' className='inline-block px-4 py-2 text-white bg-black rounded-lg hover:bg-gray-700'>
               Browse Jobs
-            </a>
+            </Link>
           </Card>
           <Card bg='bg-indigo-100'>
             <h2 className='text-2xl font-bold'>For Employers</h2>
             <p className='mt-2 mb-4'>List your job to find the perfect developer for the role</p>
-            <a
-              href='/add-job.html'
+            <Link
+              to='/add-job'
               className='inline-block px-4 py-2 text-white bg-indigo-500 rounded-lg hover:bg-indigo-600'
             >
               Add Job
-            </a>
+            </Link>
           </Card>
         </div>
       </div>
