945dd211540b8815a68eafcea1897520d060d1f3
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-04-02 16:59:23 +0300

    jobs page created

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,9 @@
+import { JobListings } from '../components/JobListings';
+
+export const JobsPage = () => {
+  return (
+    <section className='px-4 py-6 bg-blue-500'>
+      <JobListings />
+    </section>
+  );
+};
