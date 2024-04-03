52ffac6824b39b8bd3a33c9ee2868aeea3e29098
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-04-03 11:43:25 +0300

    loader and new route added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { MainLayout } from './layouts/MainLayout';
 import { HomePage } from './pages/HomePage';
 import { JobsPage } from './pages/JobsPage';
 import { NotFound } from './pages/NotFound';
+import { JobPage, jobLoader } from './pages/JobPage';
 
 const router = createBrowserRouter(
   // mainlayout sayfayi dinamik olarak gostermek icin kullaniliyor navbari heryerde gostermek istiyorum ancak geri kalani outlet esletikce gosterecegim
@@ -15,7 +16,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
-
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
       <Route path='*' element={<NotFound />} />
     </Route>
   )
