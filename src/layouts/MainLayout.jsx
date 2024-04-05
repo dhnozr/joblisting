67064ace6a4058fadeed0eec58f9e703674eb1c7
Duhan Ozarslan <r.d.ozarslan@gmail.com>
2024-04-05 14:51:04 +0300

    toast container added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { Navbar } from '../components/Navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const MainLayout = () => {
   // outlet route altinda eslesen tum routelari sayfada gostermek icin kullaniliyor.
@@ -7,6 +9,7 @@ export const MainLayout = () => {
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 };
